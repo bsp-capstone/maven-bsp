f0f64a7e60b5a39f8a01ac20ad37c99a51a5744c
gavjan <gevochobanyan@gmail.com>
2022-03-01 14:52:23 +0100

    remove old signature

--- controller/src/main/java/maven_controller/MavenControllerDemo.java
+++
@@ -1,9 +0,0 @@
-package maven_controller;
-
-public class MavenControllerDemo {
-    public static void main(String[] args) {
-        MavenController controller = new MavenController("mvn_test_project/");
-        controller.install();
-        controller.compile();
-    }
-}
